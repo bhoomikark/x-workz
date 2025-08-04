e56630b9bbbdcea46b6662308fc96f2a8e45d4b1
Bhoomika R K <bhoomikarabballige@gmail.com>
2025-08-04 23:00:37 +0530

    Performing Logics

---
+++ Logics/src/Logics.java
@@ -0,0 +1,16 @@
+public class Logics {
+    public static void main(String[] args) {
+        //Addition of 2 numbers
+        int a=10,b=20;
+        int add;
+        add=a+b;
+        System.out.println("The addition of 2 numbers is: "+add);
+
+        //Multiplication pf 3 numbers
+        int m=1,n=3,o=5;
+        int product;
+        product=m*n*o;
+        System.out.println("The product of 3 numbers is: "+product);
+    }
+
+}
