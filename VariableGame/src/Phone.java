63bf0b96543dcdfc672f95d5b9c462aabefaae1a
Bhoomika R K <bhoomikarabballige@gmail.com>
2025-08-07 23:57:50 +0530

    Local and global variable

--- VariableGame/src/Phone.java
+++ VariableGame/src/Phone.java
@@ -2,7 +2,7 @@ public class Phone {
    static int mobile_cost=20000;
   static  String model_name="Samsung S-23";
    static String mobile_color="Black";
-   static String mobile_type="Smart Phone"
+   static String mobile_type="Smart Phone";
         public static void main(String[] args) {
 
             System.out.println("The mobile cost is: "+mobile_cost);
