c5f235bbe2f6688c53ff621649bd96d025fc96ff
Bhoomika R K <bhoomikarabballige@gmail.com>
2025-08-14 00:42:12 +0530

    Accessing non static with static main method using new(logics)

---
+++ NonStatic/src/Triangle.java
@@ -0,0 +1,9 @@
+public class Triangle {
+    void area(int b , int h)
+    {
+        double result=b*h;
+        System.out.println("The area of Triangle is: "+result);
+    }
+
+
+}
---
+++ NonStatic/src/TriangleRunner.java
@@ -0,0 +1,5 @@
+public class TriangleRunner {
+    public static void main(String[] args) {
+      new  Triangle().area(10,20);
+    }
+}
